0982abc183088d59071cc126936d1b2cce4dde43
LilyPerry765 <perrylilytester@gmail.com>
2024-02-26 15:37:29 +0330

    added the project.

---
+++ dist/javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"database"},{"l":"domain"},{"l":"main"},{"l":"model"},{"l":"rsbuttom"},{"l":"service"},{"l":"view"}];updateSearchResults();
